57e022d50d8fdc5be61afe514274820350df2be7
Jun Yu <zlsl7343@gmail.com>
2025-10-13 10:11:40 +0900

    rename: spring drill로 변경

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
-rootProject.name = "spring-cloud-gateway-drill"
-include("gateway-webmvc")
-include("example-service")
-include("example-service:a-service")
-include("example-service:b-service")
-include("example-service:c-service")
-include("gateway-webflux")
-include("gateway-config")
+rootProject.name = "spring-drill"
+include("spring-cloud-gateway")
+include("spring-cloud-gateway:example-service")
+include("spring-cloud-gateway:example-service:a-service")
+include("spring-cloud-gateway:example-service:b-service")
+include("spring-cloud-gateway:example-service:c-service")
+include("spring-cloud-gateway:gateway-webmvc")
+include("spring-cloud-gateway:gateway-webflux")
+include("spring-cloud-gateway:gateway-config")
+include("spring-async")
